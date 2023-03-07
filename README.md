eaefb29b4f569482c96af1b5f0e21c7c3c60ebc2
sx9dev <sx-91@outlook.com>
2023-03-07 21:24:39 +0700

    1st commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web.sh
+Access Your Terminal From Anywhere
+[Live Site](https://web-sh.sx9.is-a.dev)
---
+++ life.js
@@ -0,0 +1,24 @@
+document.getElementById('run').onclick = () => {
+    let host = document.getElementById('host').value;
+    let pass = document.getElementById('pass').value;
+    
+    let cmd = document.getElementById('cmd').value.split(' ');
+    let exec = cmd[0];
+    let args = cmd.slice(1);
+
+    console.log(cmd, exec, args)
+
+    fetch(`http://${host}:6942/run?pass=${pass}`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            exec: exec, args: args
+        })
+    }).then(res => res.json()).then(data => {
+        alert(data.message);
+        document.getElementById('out').innerText = data.outputs;
+    });
+}
