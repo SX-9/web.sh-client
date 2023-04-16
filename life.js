47d005cd945f312b8304fd56fca959ecaebbde7d
sx9dev <sx-91@outlook.com>
2023-04-16 12:45:42 +0700

    2.0 release

--- life.js
+++ life.js
@@ -1,29 +1,52 @@
-const run = () => {
-    let host = document.getElementById('host').value;
-    let pass = document.getElementById('pass').value;
-    
-    let cmd = document.getElementById('cmd').value.split(' ');
-    let exec = cmd[0];
-    let args = cmd.slice(1);
+const $ = (el) => document.querySelector(el);
+const { host, pass, cmds } = localStorage;
+let saved = cmds ? JSON.parse(cmds) : [];
 
-    console.log(cmd, exec, args)
-
-    fetch(`http://${host}:6942/run?pass=${pass}`, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            exec: exec, args: args
-        })
-    }).then(res => res.json()).then(data => {
-        alert(data.message);
-        document.getElementById('out').innerText = data.output;
+function run(cmd) {
+	$('#out').innerText = 'Loading...';
+  fetch(`http://${host}:6942/run?pass=${pass}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(cmd),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      $("#out").innerText = data.output || data.message;
     });
 }
 
-document.getElementById('run').onclick = run;
-window.onkeypress = (e) => {
-    if (e.key === 'Enter') run();
+function create(cmd, name) {
+  added = cmd.split(" ");
+  saved.push({ cmd: added, name });
+  localStorage.setItem("cmds", JSON.stringify(saved));
+  init(saved);
 }
+
+function init(cmds) {
+  $("div").innerHTML = "";
+  cmds.forEach((cmd) => {
+    const el = document.createElement("button");
+    el.innerText = cmd.name;
+    el.onclick = () => run(cmd.cmd);
+		el.oncontextmenu = () => del(cmd.name)
+    $("div").appendChild(el);
+  });
+}
+
+function del(name) {
+	localStorage.setItem('cmds', JSON.stringify(saved.filter(obj => obj.name !== name)));
+	location.reload();
+}
+
+if (!host || !pass) $("#info").showModal();
+$("#info").onsubmit = (e) => {
+  localStorage.setItem("host", $("#host").value);
+  localStorage.setItem("pass", $("#pass").value);
+};
+
+$("#host").value = host;
+$("#pass").value = pass;
+init(saved);
